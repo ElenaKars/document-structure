5183529702b90c88f48910fd83bd640aba6ba71b
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2023-01-19 14:50:11 +0800

    update task.js

--- tooltip/task.js
+++ tooltip/task.js
@@ -1,35 +1,33 @@
 'use strict'
 
 const hasTooltipEls = Array.from(document.querySelectorAll('.has-tooltip'))
+
 let tooltipEl
 
-hasTooltipEls.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault()
-    let target = e.target
-
-    tooltipEl = document.createElement('div')
-    tooltipEl.className = 'tooltip'
-    tooltipEl.classList.add('tooltip_active')
-
-    tooltipEl.innerHTML = target.getAttribute('title')
-    target.after(tooltipEl)
-    let coords = target.getBoundingClientRect()
-    let left = coords.left + (target.offsetWidth - tooltipEl.offsetWidth) / 2
-    if (left < 0) {
-      left = 0
+document.body.addEventListener('click', (e) => {
+  e.preventDefault()
+  let tooltipDelete = document.querySelector('.tooltip')
+  let target = e.target.closest('.has-tooltip')
+  if (!target) {
+    return
+  }
+  if (tooltipDelete) {
+    tooltipDelete.remove()
+    if (target.title === tooltipDelete.innerHTML) {
+      return
     }
-    let bottom = coords.top + target.offsetHeight
-    tooltipEl.style.left = left + 'px'
-    tooltipEl.style.top = bottom + 'px'
+  }
+  tooltipEl = document.createElement('div')
+  tooltipEl.innerHTML = target.title
+  tooltipEl.className = 'tooltip tooltip_active'
+  let coords = target.getBoundingClientRect()
+  let bottom = coords.top + target.offsetHeight + 5
+  let left = coords.left + (target.offsetWidth - tooltipEl.offsetWidth) / 2
+  if (left < 0) {
+    left = 0
+  }
+  tooltipEl.style.left = left + 'px'
+  tooltipEl.style.top = bottom + 'px'
 
-    hasTooltipEls.forEach((item) => {
-      item.addEventListener('mouseout', (e) => {
-        if (tooltipEl) {
-          tooltipEl.remove()
-          tooltipEl = null
-        }
-      })
-    })
-  })
+  document.body.append(tooltipEl)
 })
